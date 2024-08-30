b882b8cdb7f4963810d93dfc76bd8df197891235
markzam <markzamora@live.com>
2024-08-30 10:54:26 -0600

    created get sample

--- React_REST_API/app.js
+++ React_REST_API/app.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dummyData from "./dummyData/dummyData.js"
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res, next)=>{
-    res.send("Hello World");
+    console.log("This is get");
+    res.send(dummyData);
+});
+
+app.post("/", (req, res, next)=>{
+    console.log("This is post");
+    res.send(dummyData);
+});
+
+app.put("/", (req, res, next)=>{
+    console.log("This is put");
+    res.send(dummyData);
+});
+
+app.delete("/", (req, res, next)=>{
+    console.log("This is delete");
+    res.send(dummyData);
 });
 
 app.listen(3000, () => {
