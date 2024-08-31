a902617968ed03b07ef50937ed8556ee821f234e
markzam <markzamora@live.com>
2024-08-30 22:35:47 -0600

    missing postedBy on create/post

--- React_REST_API/app.js
+++ React_REST_API/app.js
@@ -7,16 +7,17 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/sifts", (_, res) => {
-    console.log("This is GET");
+    console.log("-->Getting/Reading all sifts");
     res.send(dummySifts);
 });
 
 app.post("/sifts", (req, res) => {
-    console.log("This is POST");
+    console.log("-->Posting/Creating a new sift");
     const newSift = {
         ...req.body, 
+        id: dummySifts.length + 1,
         postedAt: new Date(), 
-        id: dummySifts.length + 1
+        revision: 0
     };
     
     dummySifts.push(newSift);
@@ -25,18 +26,18 @@ app.post("/sifts", (req, res) => {
 });
 
 app.put("/sifts/:siftId", (req, res) => {
-    
+    console.log("-->Putting/Updating a sift");
     const revDate = new Date
     const siftId = req.params.siftId;
     const siftToUpdate = dummySifts.find((p) => p.id === +siftId);
-    console.log(siftToUpdate.revision);
+    
 
     if(!siftToUpdate) res.status(404).send("Product/Sift not found")
     
     siftToUpdate.sift = req.body.sift;
-    //siftToUpdate.revision = siftToUpdate.revision + 1;
-    siftToUpdate.revision = {...siftToUpdate, revision: req.body.revision + 1};
-    
+    siftToUpdate.revision = siftToUpdate.revision + 1;
+    //siftToUpdate.revision = {...siftToUpdate, revision: req.body.revision + 1};
+    console.log(siftToUpdate.revision);
 
     
     //siftToUpdate.revision = {revision: req.body.revision + 1};
--- React_REST_API/dummySift/dummySifts.js
+++ React_REST_API/dummySift/dummySifts.js
@@ -5,44 +5,38 @@ const date = new Date();
 
 const dummySifts = [
     {
-      id: 1,
       sift: "Sift Number 1",
+      id: 1,
       postedBy: "User@email.com",
-      //postedAt: new Date(),
       postedAt: date,
       revision: 0,
       revDate: new Date(),
     },
     {
-      id: 2,
       sift: "Sift Number 2",
+      id: 2,
       postedBy: "User2@email.com",
-      //postedAt: new Date(),
       postedAt: date,
       revision: 0,
       revDate: new Date(),
     },
  
     {
-        id: 3,
-        sift: "Sift number 3",
-        postedBy: "anotny@email.com",
-        //postedAt: new Date(),
-        postedAt: date,
-        revision: 0,
-        revDate: new Date(),
+      sift: "Sift number 3",
+      id: 3,
+      postedBy: "anotny@email.com",
+      postedAt: date,
+      revision: 0,
+      revDate: new Date(),
     },
      
     {
-        id: 4,
-        sift: "Sift mumbo 4",
-        postedBy: "anotny@email.com",
-        postedAt: date,
-        revision: 0,
-        revDate: new Date(),
-        
-        
-        
+      sift: "Sift mumbo 4",
+      id: 4,
+      postedBy: "anotny@email.com",
+      postedAt: date,
+      revision: 0,
+      revDate: new Date(),
     },
 
     
