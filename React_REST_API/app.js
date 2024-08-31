68a7bd0df1813a2aef1b2f8add3016b65a6b5bd3
markzam <markzamora@live.com>
2024-08-30 22:12:51 -0600

    created cru method

--- React_REST_API/app.js
+++ React_REST_API/app.js
@@ -1,29 +1,49 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import dummyData from "./dummyData/dummyData.js"
+import dummySifts from "./dummySift/dummySifts.js"
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next)=>{
-    console.log("This is get");
-    res.send(dummyData);
+app.get("/sifts", (_, res) => {
+    console.log("This is GET");
+    res.send(dummySifts);
 });
 
-app.post("/", (req, res, next)=>{
-    console.log("This is post");
-    res.send(dummyData);
+app.post("/sifts", (req, res) => {
+    console.log("This is POST");
+    const newSift = {
+        ...req.body, 
+        postedAt: new Date(), 
+        id: dummySifts.length + 1
+    };
+    
+    dummySifts.push(newSift);
+
+    res.status(201).send(newSift);
 });
 
-app.put("/", (req, res, next)=>{
-    console.log("This is put");
-    res.send(dummyData);
-});
+app.put("/sifts/:siftId", (req, res) => {
+    
+    const revDate = new Date
+    const siftId = req.params.siftId;
+    const siftToUpdate = dummySifts.find((p) => p.id === +siftId);
+    console.log(siftToUpdate.revision);
 
-app.delete("/", (req, res, next)=>{
-    console.log("This is delete");
-    res.send(dummyData);
+    if(!siftToUpdate) res.status(404).send("Product/Sift not found")
+    
+    siftToUpdate.sift = req.body.sift;
+    //siftToUpdate.revision = siftToUpdate.revision + 1;
+    siftToUpdate.revision = {...siftToUpdate, revision: req.body.revision + 1};
+    
+
+    
+    //siftToUpdate.revision = {revision: req.body.revision + 1};
+    siftToUpdate.revDate = new Date;
+
+    res.send(siftToUpdate);
+
 });
 
 app.listen(3000, () => {
---
+++ React_REST_API/dummySift/dummySifts.js
@@ -0,0 +1,51 @@
+const date = new Date();
+//const currentMonth = date.getMonth();
+
+//date.setMonth(currentMonth - 3);
+
+const dummySifts = [
+    {
+      id: 1,
+      sift: "Sift Number 1",
+      postedBy: "User@email.com",
+      //postedAt: new Date(),
+      postedAt: date,
+      revision: 0,
+      revDate: new Date(),
+    },
+    {
+      id: 2,
+      sift: "Sift Number 2",
+      postedBy: "User2@email.com",
+      //postedAt: new Date(),
+      postedAt: date,
+      revision: 0,
+      revDate: new Date(),
+    },
+ 
+    {
+        id: 3,
+        sift: "Sift number 3",
+        postedBy: "anotny@email.com",
+        //postedAt: new Date(),
+        postedAt: date,
+        revision: 0,
+        revDate: new Date(),
+    },
+     
+    {
+        id: 4,
+        sift: "Sift mumbo 4",
+        postedBy: "anotny@email.com",
+        postedAt: date,
+        revision: 0,
+        revDate: new Date(),
+        
+        
+        
+    },
+
+    
+    ];
+
+    export default dummySifts;
