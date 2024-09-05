9655b398c28f4e1cdb6391b53c60ab93a9974177
markzam <markzamora@live.com>
2024-09-05 16:05:28 -0600

    before deleting dumm data this is working with the dummy

--- React_REST_API/models/Sift.js
+++ React_REST_API/models/Sift.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const siftSchema = new mongoose.Schema({
-    name: { type: String, require: true},
-    sift: { type: String, require: true},
-    postedAt: {type: Date, require: true},
-    sift_r: {type: String, require: true},
-    revision: {type: Number, require: true},
-    date_r: {type: Date, require: true},
+    name: { type: String, required: true},
+    sift: { type: String, required: true},
+    postedAt: {type: Date, required: true},
+    sift_r: {type: String, required: true},
+    revision: {type: Number, required: true},
+    date_r: {type: Date, required: true},
     ownerId: {mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
