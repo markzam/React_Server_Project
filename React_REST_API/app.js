e127b6d393bc5d8e8622762980f634deeb6d5765
markzam <markzamora@live.com>
2024-09-05 15:24:01 -0600

    install mongodb and env

--- React_REST_API/app.js
+++ React_REST_API/app.js
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser';
 import cors from "cors";
 import siftsRouter from './routers/siftsRouter.js';
 import usersRouter from './routers/usersRouter.js';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
 
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173/"
+    origin: process.env.UI_URL
 }));
 
 app.use(bodyParser.json());
@@ -16,6 +19,12 @@ app.use("/sifts", siftsRouter);
 
 app.use("/users", usersRouter);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(()=> {
+    console.log("Successfully connected to the database");
+    const port = process.env.PORT ?? 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch(e=>console.error(`Unable to connect to the database with error: ${e}`));
+
+
